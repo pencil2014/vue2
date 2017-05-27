6ce1d6c1a36dc3b022070dc877b263dd3661ae74
以梦为马 <532079124@qq.com>
2017-05-27 18:34:54 +0800

    update

--- src/components/partner/info.vue
+++ src/components/partner/info.vue
@@ -257,7 +257,7 @@ export default {
 
 
 .showpicbox {position: fixed; left: 0; top: 0; right: 0; bottom: 0; background-color: rgba(0,0,0,0.2); width: 100%; height: 100%; z-index: 3;}
-.showpicbox-cnt { width: 85%; height: 80%;  margin: auto; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);}
+.showpicbox-cnt { width: 90%; height: 70%;  margin: auto; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);}
 .showpicbox-cnt .mint-swipe-items-wrap { position: relative; height: 100%; }
 .showpicbox-cnt .mint-swipe-item{ text-align: center; height: 80%; background-color: #eee;}
 .showpicbox-cnt img {position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);}
